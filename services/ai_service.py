e4db7fde747cb522e39e8e3b8dd3efd3fa88dd4d
harmanrathi1556 <harmanrathi1556@gmail.com>
2026-03-29 21:27:27 +0530

    Update ai_service.py

--- services/ai_service.py
+++ services/ai_service.py
@@ -23,11 +23,14 @@ def solve_doubt(question):
 
         response = requests.post(url, json=payload, headers=headers)
 
+        print("RAW RESPONSE:", response.text)
+
         data = response.json()
 
-        answer = data['candidates'][0]['content']['parts'][0]['text']
+        if "candidates" not in data:
+            return "AI Error: " + str(data)
 
-        return answer
+        return data['candidates'][0]['content']['parts'][0]['text']
 
     except Exception as e:
-        return f"Error: {str(e)}"
+        return f"AI Error: {str(e)}"
